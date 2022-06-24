2a3f2f397c7c917556e5ec9b646f00150f6e5bda
Loc Nguyen <vinhloc30796@gmail.com>
2022-06-24 17:22:05 +0700

    Add modules to main.rs

--- 2.lesson/hands-on/src/main.rs
+++ 2.lesson/hands-on/src/main.rs
@@ -1,7 +1,11 @@
 mod data_types;
 mod flow_control;
+mod person;
+mod pi_naive;
 mod functions;
 mod ownership;
+mod option;
+mod ok_err;
 mod shadowing;
 mod string_literal;
 mod string_obj;
@@ -9,7 +13,7 @@ mod structure;
 mod variable;
 
 fn main() {
-    data_types::print_data_types();
+    // data_types::print_data_types();
     // string_literal::print_string_literal();
     // string_obj::print_string_obj();
     // variable::print_string_obj();
@@ -18,4 +22,8 @@ fn main() {
     // flow_control::flow_control();
     // flow_control::ternal_like();
     // ownership::ownership();
+    // person::main();
+    // pi_naive::main();
+    // option::main();
+    ok_err::main()
 }
