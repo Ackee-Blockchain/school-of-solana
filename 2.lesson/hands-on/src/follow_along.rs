2a1492abe4b0c871d23036a12a2b45d88027adc0
Loc Nguyen <vinhloc30796@gmail.com>
2022-06-24 16:53:34 +0700

    Add follow_along.rs

---
+++ 2.lesson/hands-on/src/follow_along.rs
@@ -0,0 +1,19 @@
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person{
+    fn new_person(first_name: &str, last_name: &str) -> Person {
+        Person {
+            first_name: first_name.to_string(),
+            last_name: last_name.to_string(),
+        }
+    }
+}
+
+pub fn follow() {
+    let person: Person = Person::new_person("John", "Doe");
+
+    println!("Person: {} {}", person.first_name, person.last_name);
+}
