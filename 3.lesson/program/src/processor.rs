a9fac88045aa391fac36ff8d9d386107c357adb2
Vladimír Marcin <marcinvlado@gmail.com>
2022-07-06 10:47:56 +0200

    ✨ 4.lesson - PDAs, invoke_signed

--- 3.lesson/program/src/processor.rs
+++ 3.lesson/program/src/processor.rs
@@ -2,11 +2,11 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    program::invoke,
+    program::{invoke, invoke_signed},
     pubkey::Pubkey,
     rent::Rent,
-    system_instruction,
-    sysvar::Sysvar,
+    system_instruction, system_program,
+    sysvar::Sysvar, native_token::sol_to_lamports,
 };
 
 use crate::{instruction::TurnstileInstruction, state::State};
@@ -35,6 +35,12 @@ pub fn initialize(
     let account_into_iter = &mut accounts.iter();
     let state_account_info = next_account_info(account_into_iter)?;
     let initialezer_account_info = next_account_info(account_into_iter)?;
+    let treasury_account_info = next_account_info(account_into_iter)?;
+
+    let (treasury, bump) =
+        Pubkey::find_program_address(&[initialezer_account_info.key.as_ref()], program_id);
+
+    assert_eq!(treasury, *treasury_account_info.key);
 
     let rent = Rent::get()?;
     invoke(
@@ -51,12 +57,40 @@ pub fn initialize(
     let state = State { locked: init_state };
     state.serialize(&mut *state_account_info.data.borrow_mut())?;
 
+    invoke_signed(
+        &system_instruction::create_account(
+            &initialezer_account_info.key,
+            &treasury,
+            rent.minimum_balance(0),
+            0,
+            &system_program::id(),
+        ),
+        &[
+            initialezer_account_info.clone(),
+            treasury_account_info.clone(),
+        ],
+        &[&[initialezer_account_info.key.as_ref(), &[bump]]],
+    )?;
+
     Ok(())
 }
 
 pub fn coin(_program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let account_into_iter = &mut accounts.iter();
     let state_account_info = next_account_info(account_into_iter)?;
+    let treasury_account_info = next_account_info(account_into_iter)?;
+    let user_account_info = next_account_info(account_into_iter)?;
+
+    assert_eq!(user_account_info.is_signer, true);
+    
+    invoke(
+        &system_instruction::transfer(
+            &user_account_info.key,
+            &treasury_account_info.key,
+            sol_to_lamports(1.0)
+        ),
+        &[user_account_info.clone(), treasury_account_info.clone()],
+    )?;
 
     let state = State { locked: false };
     state.serialize(&mut *state_account_info.data.borrow_mut())?;
