4c450891571da669e06de7f6d3265b89f412dc70
lukacan <andrej.lukacovic98@gmail.com>
2024-05-03 11:42:21 +0200

    ✨ Update Lesson 1

---
+++ 1.lesson/hello_world/README.md
@@ -0,0 +1,14 @@
+# Hello World!
+This is a Hello World program for the School of Solana
+
+## To Test it out
+If you have everything installed from the Dev-Setup, run:
+
+1.
+    ```bash
+    yarn install
+    ```
+2.
+    ```bash
+    anchor test
+    ```
---
+++ 1.lesson/hello_world/programs/hello_world/src/lib.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+
+declare_id!("J9TXPkyYXLw82XsedHXVq8nMsso8copxRtmpz3oWsoMJ");
+
+#[program]
+pub mod hello_world {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let hello_world_account = &mut ctx.accounts.hello_world_account;
+        hello_world_account.greeting = "Hello World".to_string();
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 50,
+    )]
+    pub hello_world_account: Account<'info, HelloWorldAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct HelloWorldAccount {
+    pub greeting: String,
+}
